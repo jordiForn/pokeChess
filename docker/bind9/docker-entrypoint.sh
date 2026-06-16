040cff5da7a3b69ef56e9404891af06a6f77e706
jordiForn <thejfk18@gmail.com>
2026-06-17 00:16:37 +0200

    fallo contenedor dns

---
+++ docker/bind9/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+echo "[dns] Validando configuración Bind9..."
+named-checkconf /etc/bind/named.conf
+named-checkzone app.local /etc/bind/zones/db.app.local
+
+echo "[dns] Iniciando named (puerto 53 TCP/UDP)..."
+exec /usr/sbin/named -u bind -f -c /etc/bind/named.conf -L /var/log/bind/default.log
